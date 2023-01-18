c42de96564aad457c233c689d00c9beca2c8222f
Zhenlin Wang <48540265+Criss-Wang@users.noreply.github.com>
2023-01-17 22:48:19 -0500

    Update constants.go

--- src/travel-planner/constants/constants.go
+++ src/travel-planner/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	CHATGPT_API_KEY     = "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"
+	CHATGPT_API_KEY     = "xxx"
 	TRIPADVISOR_API_KEY = "62A808FFA5BB43458AA517B597F7C0E1"
 	MYSQL_CONFIG        = "charset=utf8mb4&parseTime=True&loc=Local"
 	MYSQL_DBNAME        = "travel_planner"
